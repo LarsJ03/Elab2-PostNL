14a7686aebb2ae2e7c3ec846b05a74692144872d
Pantso365 <p.avgerinopoulos@student.maastrichtuniversity.nl>
2024-06-01 16:41:25 +0200

    Final fixes. IMPORTANT: you need the new distances file to run this or to run the RoadNetwork inside filecreation

--- Java/Maastricht Network/Network/LinearData.java
+++ Java/Maastricht Network/Network/LinearData.java
@@ -7,15 +7,20 @@ public class LinearData {
     	 
     	 for(ServiceLocation location : servicelocations) {
     		 for(Road road : roads) {
-    			 if(road.getAssignedFacility() == location.getLocationId()) {
-    				 location.addtotalKM(road.getDist());
+    			 if(road.getType().equals("residential") || road.getType().equals("service") ||  road.getType().equals("living_street") &&
+    					 road.getPopulation() > 1 && Double.isNaN(road.getPopulation()) == false ) {
+    				 if(road.getAssignedFacility() == location.getLocationId()) {
+    					 location.addtotalKM(road.getDist());
+    				 }
     			 }
     		 }
     	 }
     	 
     	 for(ServiceLocation location : servicelocations) {
     		 for(Road road : roads) {
-    			 if(road.getType().equals("residential") || road.getType().equals("service") ||  road.getType().equals("living_street") && road.getPopulation() > 0) {
+    			 if((road.getType().equals("residential") || road.getType().equals("service") ||  road.getType().equals("living_street")) &&
+    					 road.getPopulation() > 1.0 && Double.isNaN(road.getPopulation()) == false ) {
+    				 System.out.println(road.getPopulation());
     				 if(road.getAssignedFacility() == location.getLocationId()) {
     					 road.setPackages( (int) (((location.getTotalDeliveries()+location.getTotalPickups())/location.getTotalKM()) * road.getDist()));
     				 }
